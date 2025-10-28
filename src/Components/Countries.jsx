e3f3f8b3af14cccc365f44dc36c2106e4b41131b
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-29 00:29:42 +0600

    Visited Country List

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -3,19 +3,44 @@ import Country from './Country'
 
 export default function Countries() {
 
-    const [courtries, setCountries] = useState([])
+    const [courtries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([])
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/independent?status=true')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
 
+    const handleVisitedCountry = (country) => {
+  const exists = visitedCountries.find(c => c.cca3 === country.cca3);
+
+
+  if (exists) {
+    // remove from list
+    const remaining = visitedCountries.filter(c => c.cca3 !== country.cca3);
+    setVisitedCountries(remaining);
+  } else {
+    // add to list
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  }
+};
+
   return (
     <div >
         <h1 className='text-xl font-semibold'>Counrties: {courtries.length} </h1>
+        <div>
+          <h5>Visited Countries </h5>
+          <ul>
+            {
+              visitedCountries.map(country=> <li key= {country.cca3} >  {  country.name.common}</li> )
+            }
+          </ul>
+        </div>
         <div className=' grid grid-cols-1 md:grid-cols-3 gap-5 '>
         {
-            courtries.map((country, ind) =><Country key={ind} country={country}></Country>)
+            courtries.map((country, ind) =><Country key={ind} country={country} handleVisitedCountry={handleVisitedCountry} ></Country>)
         }
         </div>
     </div>
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function Country({country}) {
+export default function Country({country, handleVisitedCountry}) {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisted] = useState(false);
@@ -10,14 +10,18 @@ export default function Country({country}) {
     }
 
   return (
-    <div className='border-2 text-start  border-green-600 p-4  '>
+    <div className={`border-2  text-start  border-green-600 p-4 ${visited && 'bg-green-500'} `}>
         <h1 className='text-2xl font-bold' >Name: {name?.common} </h1>
         <img className='w-32' src={flags?.png} alt="" />
         <h1 className='text-2xl font-bold'  >Poputation: {population} </h1>
         <h1 className='text-2xl font-bold'  >Area: {area} </h1>
         <p className='text-2xl font-semibold'><small>Code: {cca3} </small></p>
-        <button onClick={handleVisited} className='btn btn-primary'>{visited? 'Visited' : 'Going'}</button>
-        {visited && 'I have Visited this Country'}
+        <button onClick={() => {
+          handleVisited();
+          handleVisitedCountry(country)
+        }} className='btn btn-primary'>{visited? 'Visited' : 'Going'}</button>
+        {visited ? 'I have Visited this Country' : 'I want to visit'}
+        
     </div>
   )
 }
